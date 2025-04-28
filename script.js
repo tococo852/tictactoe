ea052d8036fb29d191fd080ef1178754ae3872e8
tococo852 <tococo852@gmail.com>
2025-04-28 13:52:49 -0400

    added player object methods, small game display

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
+function createPlayer(name,symbol){
+    wins=0;
+    ties=0;
+    losses=0;
+    const getName= () => {return name}
+    const getSymbol= () => {return symbol}
+    const increaseWins= () =>{wins++}
+    const getWins= () => {return wins}
+    const increaseTies= () =>{ties++}
+    const getTies= () => {return ties}
+    const increseLosses= () =>{losses++}
+    const getLosses= () => {return losses}
+    const getTotalGames = () =>{return losses+wins+wins}
+    return {increaseWins,getWins,increaseTies,getTies,increseLosses,getLosses,getTotalGames,getName,getSymbol}
+}
 
 const gameBoard = (function(){
     //tiles have the owner player if any and its location in the array
     function createTile(xin,yin){
         let playerOwn=NaN
+        let playerIcon="?"
 
 
-        setOwner=(playerName)=>{
-            playerOwn=playerName}
+        setOwner=(player)=>{
+            playerOwn=player.getName()
+            playerIcon=player.getSymbol()
+        }
         getLocation= () =>{
             return [xin,yin]
         };
-        getOwner = () =>{return playerOwn}
-        return {getLocation,setOwner,getOwner}
+        getOwner = () =>{ return playerOwn}
+        getIcon= () =>{ return playerIcon}
+        return {getLocation,setOwner,getOwner, getIcon}
     };
-    //below fuctions makes the array
+    //below fuctions makes the array board and fills it
     let board=[];
     (function(){
         for (let i= 0; i < 3; i++) {
@@ -25,8 +44,16 @@ const gameBoard = (function(){
     })();
 
 
-    const modifyTile= (player, x, y) => {board[x][y].setPlayer(player);};
+    const getTile= ( x, y) => {return board[x][y];};
+    const setTile= (player,x,y) => {board[x][y].setOwner(player)}
 
+    const displayGrid = () => {
+        for (let index = 0; index < board.length; index++) {
+            console.log(`|| ${board[index][0].getIcon()} || ${board[index][1].getIcon()} || ${board[index][2].getIcon()} ||`)
+
+            
+        }
+    }
     const calculateWin= () => {
         board.forEach( (row,x)=> {
                 row.forEach((_,y) => {
@@ -36,20 +63,15 @@ const gameBoard = (function(){
         });
         
     };
-    return {modifyTile,calculateWin,createTile,board}
+    return {board,getTile,setTile,calculateWin,displayGrid}
 })();
 
-function CreatePlayer(name){
-    wins=0;
-    ties=0;
-    losses=0;
-    const increaseWins= () =>{wins++}
-    const getWins= () => {wins}
-    const increaseTies= () =>{ties++}
-    const getTies= () => {ties}
-    const increseLosses= () =>{losses++}
-    const getLosses= () => {losses}
-    const getTotalGames = () =>{losses+wins+wins}
-    return {name,increaseWins,getWins,increaseTies,getTies,increseLosses,getLosses,getTotalGames}
-}
-gameBoard.calculateWin();
+player1=createPlayer("Player1", "O")
+player2=createPlayer("player2", "X")
+gameBoard.setTile(player1,0,0)
+gameBoard.setTile(player2,1,1)
+gameBoard.setTile(player1,2,2)
+console.log(player1.getSymbol())
+
+console.log(gameBoard.board[0][0].getOwner())
+gameBoard.displayGrid();
