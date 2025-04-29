c71f53f5b6a82c0eae01c289788a8a8606589d1f
tococo852 <tococo852@gmail.com>
2025-04-29 12:33:17 -0400

    added checkWin, checks board for a winning line, updates gameboard winning line if there is one and returns true or false depending if there is actually a winning line on the board at the moment of usage

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function createPlayer(name,symbol){
 
 const gameBoard = (function(){
     //tiles have the owner player if any and its location in the array
+    winningLine=[]
     function createTile(xin,yin){
         let playerOwn=NaN
         let playerIcon="?"
@@ -34,14 +35,16 @@ const gameBoard = (function(){
     };
     //below fuctions makes the array board and fills it
     let board=[];
-    (function(){
+    const generateBoard= () =>{
+        this.board=[]
         for (let i= 0; i < 3; i++) {
             board.push([]);
             for(let c=0; c<3; c++){
                 board[i].push(createTile(i,c))
             }  
         }
-    })();
+    };
+    const getBoard= ()=> {return board}
 
 
     const getTile= ( x, y) => {return board[x][y];};
@@ -54,24 +57,43 @@ const gameBoard = (function(){
             
         }
     }
-    const calculateWin= () => {
-        board.forEach( (row,x)=> {
-                row.forEach((_,y) => {
-                    console.log(board[x][y])
-                    
-                });
-        });
+    const checkWin= () => {
+        //check horizontal lines
+        for (let i = 0; i < 3; i++) {
+            if(getTile(i,0).getOwner()==getTile(i,1).getOwner() && getTile(i,1).getOwner()==getTile(i,2).getOwner()){
+                winningLine=[getTile(i,0), getTile(i,1), getTile(i,2)]
+                return true
+            }      
+        }
+        // check vertical lines
+        for (let i = 0; i < 3; i++) {
+            if(getTile(0,i).getOwner()==getTile(1,i).getOwner() && getTile(1,i).getOwner()==getTile(2,i).getOwner()){
+                winningLine=[getTile(0,i), getTile(1,i), getTile(2,i)]
+                return true
+            }      
+        }
+        //check diagonals
+        if (getTile(0,0).getOwner()==getTile(1,1).getOwner() && getTile(1,1).getOwner()==getTile(2,2).getOwner()){ 
+            winningLine=[getTile(0,0),getTile(1,1),getTile(2,2)]
+            return true}
+        if (getTile(0,2).getOwner()==getTile(1,1).getOwner() && getTile(1,1).getOwner()==getTile(2,0).getOwner()){ 
+            winningLine=[getTile(0,2),getTile(1,1),getTile(2,0)]
+            return true}
+        return false
         
     };
-    return {board,getTile,setTile,calculateWin,displayGrid}
+    const getWinningLine= () =>{return winningLine}
+    return {getBoard,getTile,setTile,checkWin,displayGrid, generateBoard,getWinningLine}
 })();
 
+//next, make game object, should carry all game logic and interaction beetwen player and board
+gameBoard.generateBoard()
 player1=createPlayer("Player1", "O")
 player2=createPlayer("player2", "X")
 gameBoard.setTile(player1,0,0)
-gameBoard.setTile(player2,1,1)
+gameBoard.setTile(player1,1,1)
 gameBoard.setTile(player1,2,2)
 console.log(player1.getSymbol())
 
-console.log(gameBoard.board[0][0].getOwner())
+console.log(gameBoard.getBoard()[0][0].getOwner())
 gameBoard.displayGrid();
